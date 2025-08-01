ea29c762caca67acf6f157be77b24cd3b52ea942
Shin-kyoto <aquashin0202@gmail.com>
2025-08-01 20:19:43 +0900

    feat(autoware_tensorrt_vad): split tensor.hpp into tensor.cpp and tensor.hpp
    Signed-off-by: Shin-kyoto <aquashin0202@gmail.com>

--- AV-Solutions/vad-trt/app/autoware_tensorrt_vad/include/autoware/tensorrt_vad/tensor.hpp
+++ AV-Solutions/vad-trt/app/autoware_tensorrt_vad/include/autoware/tensorrt_vad/tensor.hpp
@@ -15,9 +15,10 @@
  * limitations under the License.
  */
 
-#ifndef _TENSOR_H_
-#define _TENSOR_H_
+#ifndef _TENSOR_HPP_
+#define _TENSOR_HPP_
 
+#include <string>
 #include <memory>
 #include <vector>
 #include <unordered_map>
@@ -29,26 +30,8 @@
 
 namespace autoware::tensorrt_vad {
 
-inline unsigned int getElementSize(nvinfer1::DataType t) {
-  switch (t) {
-    case nvinfer1::DataType::kFLOAT:
-      return sizeof(float);
-    case nvinfer1::DataType::kHALF:
-      return sizeof(float) / 2;
-    case nvinfer1::DataType::kINT8:
-      return sizeof(int8_t);
-    case nvinfer1::DataType::kINT32:
-      return sizeof(int32_t);
-    case nvinfer1::DataType::kBOOL:
-      return sizeof(bool);
-    case nvinfer1::DataType::kUINT8:
-      return sizeof(uint8_t);
-    case nvinfer1::DataType::kFP8:
-      return sizeof(float) / 4;
-    default:
-      return 0;
-  }
-}
+// Function to get element size for different data types
+unsigned int getElementSize(nvinfer1::DataType t);
 
 struct Tensor {
   std::string name;
@@ -56,57 +39,20 @@ struct Tensor {
   nvinfer1::Dims dim;
   int32_t volume = 1;
   nvinfer1::DataType dtype;
-  // nvinfer1::TensorIOMode iomode;
 
-  Tensor(std::string name, nvinfer1::Dims dim, nvinfer1::DataType dtype): 
-    name(name), dim(dim), dtype(dtype) 
-  {
-    if( dim.nbDims == 0 ) {
-      volume = 0;
-    } else {
-      volume = 1;
-      for(int i=0; i<dim.nbDims; i++) {
-          volume *= dim.d[i];
-      }
-    }
-    cudaMalloc(&ptr, volume * getElementSize(dtype));
-  }
+  // Constructor
+  Tensor(std::string name, nvinfer1::Dims dim, nvinfer1::DataType dtype);
 
-  int32_t nbytes() {
-    return volume * getElementSize(dtype);
-  }
+  // Get number of bytes
+  int32_t nbytes();
 
+  // Load data from host to device
   template<class Dtype=float>
-  void load(const std::vector<float>& data, cudaStream_t stream = 0) {
-    if (static_cast<int32_t>(data.size()) != volume) {
-      // Note: Error logging should be handled by caller std::cerr << "Data size mismatch: expected " << volume << ", got " << data.size() << std::endl;
-      return;
-    }
-    
-    size_t dsize = volume * getElementSize(dtype);
-    
-    if (dtype == nvinfer1::DataType::kFLOAT) {
-      // Direct copy for float data
-      cudaMemcpyAsync(ptr, data.data(), dsize, cudaMemcpyHostToDevice, stream);
-    } else {
-      // Type conversion needed
-      std::vector<char> buffer(dsize);
-      Dtype* dbuffer = reinterpret_cast<Dtype*>(buffer.data());
-      
-      for (int i = 0; i < volume; i++) {
-        dbuffer[i] = static_cast<Dtype>(data[i]);
-      }
-      
-      cudaMemcpyAsync(ptr, buffer.data(), dsize, cudaMemcpyHostToDevice, stream);
-    }
-  }
+  void load(const std::vector<float>& data, cudaStream_t stream = 0);
 
+  // Copy data from device to host
   template<class T>
-  std::vector<T> cpu() {
-    std::vector<T> buffer(volume);
-    cudaMemcpy(buffer.data(), ptr, volume * sizeof(T), cudaMemcpyDeviceToHost);
-    return buffer;
-  }
+  std::vector<T> cpu();
 
 }; // struct Tensor
 
@@ -114,4 +60,4 @@ using TensorMap = std::unordered_map<std::string, std::shared_ptr<Tensor>>;
 
 } // namespace autoware::tensorrt_vad
 
-#endif // _TENSOR_H_
+#endif // _TENSOR_HPP_
